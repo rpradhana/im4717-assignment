7ebb0f10886406c70190ebc32ee18f9f68cd8743
rmonap <raymond.pradhana@gmail.com>
2017-10-27 18:31:01 +0800

    add support page

---
+++ support.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<?php include './php/head.php'; ?>
+<body class="debug o f h d">
+	<?php include './php/nav.php' ?>
+	<div class="container">
+		<div class="row">
+			<div class="two column"></div>
+			<div class="eight column">
+				<section class="support">
+					<h2 class="header">Help Centre</h2>
+					<section class="support__section" id="shipping-delivery">
+						<a href="#shipping-delivery" class="anchor"><h3>Shipping and Delivery</h3></a>
+						<table class="u-fill">
+							<tr class="table__row">
+								<th>
+									Type
+								</th>
+								<th>
+									Cost
+								</th>
+								<th>
+									<span class="u-no-wrap">Shipping Time</span>
+								</th>
+								<th>
+									<span class="u-no-wrap">Availability</span>
+								</th>
+							</tr>
+							<tr class="table__row">
+								<td>
+									Standard
+								</td>
+								<td>
+									$6.00
+								</td>
+								<td>
+									1 - 3 working days
+								</td>
+								<td>
+									<span class="u-no-wrap">Mondays - Fridays</span><br>
+									<span class="u-no-wrap">Not available on Public Holidays</span>
+								</td>
+							</tr>
+							<tr class="table__row">
+								<td>
+									Express
+								</td>
+								<td>
+									$18.00
+								</td>
+								<td>
+									Next day
+								</td>
+								<td>
+									<span class="u-no-wrap">Available all day</span>
+								</td>
+							</tr>
+						</table>
+						<p>
+							We follow local time in Singapore SGT (UTC+7). Any order made before 11.59 p.m. will be counted as day zero for its delivery time.
+						</p>
+					</section>
+					<section class="support__section" id="return-policy">
+						<a href="#return-policy" class="anchor"><h3>Return Policy</h3></a>
+						<p>
+							You can, within 14 days of the order being received, return any goods in saleable condition.
+						</p>
+						<p>
+							To process your return, please contact our customer center at +65 9876 5432 or contact@company.com or follow the instructions found at the back of the invoice that was sent with your order.
+						</p>
+						<p>
+							You simply need to fill out the Return form (included at the bottom of your invoice), insert it into your return package, receive and print out the Shipping Label sent by our customer center via email, stick the label on a return package, and then drop off at any Post Office or POPSTATION nearby. Please allow 10~14 business days for us to process your return after we receive the items.</p>
+						</p>
+					</section>
+					<section class="support__section" id="not-collected">
+						<a href="#not-collected" class="anchor"><h3>How do I receive my orders if I am not home?</h3></a>
+						<p>
+							All orders are delivered through Singapore Post and will require a signature upon delivery. If you are not at home to receive your items, a notification card will be dropped in your mailbox to invite you to collect your parcel at the nearest Singapore Post office.
+						</p>
+					</section>
+					<section class="support__section" id="help">
+						<a href="#help" class="anchor"><h3>I need help!</h3></a>
+						<p>
+							Feel free to <a href="./contact.php">contact us</a> and let us know how we could help you! We will reply to your query within 1 working day.
+						</p>
+						<p>
+							Your satisfaction is our utmost priority.
+						</p>
+					</section>
+				</section>
+			</div>
+		</div>
+		<div class="two column"></div>
+	</div>
+	<?php include './php/footer.php' ?>
+	<script type="text/javascript" src='./js/global.js'></script>
+</body>
+</html>
