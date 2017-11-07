72b26c073cc7ec4883bfcaed6a757660376d0ae2
ferdianw <ferdian001@e.ntu.edu.sg>
2017-11-08 00:55:22 +0800

    add outerwear and shirts

---
+++ sql/menotwr.sql
@@ -0,0 +1,184 @@
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Comfort Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (121, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (121, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (121, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (121, "Gray", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("MEN Comfort Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (122, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Corduroy Tracker Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (123, "Brown", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (123, "Brown", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (123, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (123, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Denim Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (124, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (124, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (124, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (124, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Down Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Green", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Green", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Beige", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (125, "Beige", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Dry Stretch Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+79.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Blue", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Beige", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (126, "Beige", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Dry-EX Ultra Stretch Full-Zip Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+49.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (127, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("MEN Dry-EX Ultra Stretch Full-Zip Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+49.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (128, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Fleece Full-Zip Long Sleeve Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+49.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Black", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Black", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "White", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "White", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Brown", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Brown", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Red", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (129, "Red", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("IDLF Corduroy Jackett", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+49.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (130, "Green", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (130, "Green", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (130, "Green", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (130, "Green", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("IDLF Tweed Jacket", "OTWR", "M",
+                             "Soft, light, wrinkle-resistant! Our stylish comfort jacket is great for casual wear.\n
+- Casual design with lower sleeve cap, no waist darts, narrow lapels, and less-visible stitching.\n
+- Updated for a more form-fitting cut.\n
+- Great for business wear, or paired with jogger pants or denim for an active style.\n
+- Stretchy jersey material allows easy movement.\n
+- Unlined for a light feel, and wrinkle-resistant so you can carry it with you in a handbag.\n
+- Added DRY technology dries sweat quickly.",
+49.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (131, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (131, "Gray", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (131, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (131, "Blue", "L", 3);
---
+++ sql/menshrt.sql
@@ -0,0 +1,162 @@
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Checked Stretch Slim Fit Long Sleeve Shirt", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (142, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (142, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (142, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (142, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Checked Stretch Slim Fit Long Sleeve Shirt 2", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (143, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (143, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (143, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (143, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Checked Stretch Slim Fit Long Sleeve Shirt 3", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (144, "Gray", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (144, "Gray", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (144, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (144, "Gray", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Checked Stretch Slim Fit Long Sleeve Shirt 4", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "Blue", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "White", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "White", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "White", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (145, "White", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Checked Stretch Slim Fit Long Sleeve Shirt 5", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "Blue", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "White", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "White", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "White", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (146, "White", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Striped Stretch Slim Fit Long Sleeve Shirt", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (147, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (147, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (147, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (147, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Striped Stretch Slim Fit Long Sleeve Shirt 2", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (148, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (148, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (148, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (148, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Striped Stretch Slim Fit Long Sleeve Shirt 3", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (149, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (149, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (149, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (149, "Blue", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Striped Stretch Slim Fit Long Sleeve Shirt 4", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (150, "Gray", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (150, "Gray", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (150, "Gray", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (150, "Gray", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Stretch Slim Fit Oxford Long Sleeve Shirt", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Blue", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "White", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "White", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "White", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "White", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Pink", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Pink", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Pink", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (151, "Pink", "L", 3);
+
+INSERT INTO products (name, category, gender, description, price, discount) VALUES ("Easy Care Stretch Slim Fit Oxford Long Sleeve Shirt 2", "SHRT", "M",
+                             "This slim-fit shirt features the highest quality fabric, sewing, finish, and design.\n
+- With added stretch for comfort and ease of movement.\n
+- Made from a premium two-ply yarn.\n
+- Easy Care processing means it stays wrinkle-resistant even after washing.\n
+- The button-down collar looks good with or without a tie.",
+39.90, 0);
+
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "Blue", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "Blue", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "Blue", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "Blue", "L", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "White", "S", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "White", "M", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "White", "XL", 3);
+INSERT INTO inventory (productsID, color, size, stock) VALUES (152, "White", "L", 3);
